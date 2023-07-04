20ca159f227ac9062e1cefc5bdacc0b880bdb8a6
koke <jorgetambleygomez@gmail.com>
2023-07-04 10:50:32 -0400

    1

--- client/src/components/views/HomePage/HomePage.jsx
+++ client/src/components/views/HomePage/HomePage.jsx
@@ -1,1 +1,10 @@
-const HomePage =()=>{}
+import React from 'react';
+import styles from "./HomePage.module.css"
+
+const HomePage = () => {
+  return (
+    <h1 className={styles.h1}>Homepage</h1>
+  );
+}
+
+export default HomePage;
