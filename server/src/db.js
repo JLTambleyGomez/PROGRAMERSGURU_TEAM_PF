1bbb7f35c9075a11203c55eae5e2b7eb5dbcdd4a
FPassetti <franciscopassettimail.com>
2023-07-05 10:11:11 -0300

    deploy db

--- server/src/db.js
+++ server/src/db.js
@@ -7,7 +7,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;  
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/finalproject`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/railway`, {
   logging: false, 
   native: false, 
 });
