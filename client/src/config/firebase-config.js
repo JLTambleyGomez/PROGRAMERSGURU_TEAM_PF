28346f36bf7056df380623d20c49f7ec1a8c6d2a
FPassetti <franciscopassettimail.com>
2023-07-11 19:59:34 -0300

    login con google

---
+++ client/src/config/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfIYfBXFHQYwd8edQwo-0UjC9TU3KXVsE",
+  authDomain: "programmers-guru.firebaseapp.com",
+  projectId: "programmers-guru",
+  storageBucket: "programmers-guru.appspot.com",
+  messagingSenderId: "505462352111",
+  appId: "1:505462352111:web:322bf2260244ad7be3531a"
+};
+
+const app = initializeApp(firebaseConfig);
