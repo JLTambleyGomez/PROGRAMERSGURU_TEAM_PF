8e35cd107a97aef18f637f1206ade2fb1ebaeb56
koke <jorgetambleygomez@gmail.com>
2023-07-18 16:56:58 -0400

    martes2

--- client/src/components/views/HomePage/HomePage.jsx
+++ client/src/components/views/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "../../../config/firebase-config";
@@ -15,7 +16,7 @@ import s from "./HomePage.module.css";
 import CoursesPreview from "../../datos/CoursesPreview/CoursesPreview";
 import Comments from "../../datos/Comments/Comments";
 import SubscripcionesFlotante from "../../datos/Subscripciones/SubscripcionesFlotante";
-
+import axios from "axios"
 //_________________________module_________________________
 function HomePage () {
 
