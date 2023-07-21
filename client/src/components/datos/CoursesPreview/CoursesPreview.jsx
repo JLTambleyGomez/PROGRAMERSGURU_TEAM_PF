37beafa8d3d74219113b6492645489c39ab2e7a9
SebastianMartel <martel.ssebastian@gmail.com>
2023-07-21 06:55:15 -0500

    last

--- client/src/components/datos/CoursesPreview/CoursesPreview.jsx
+++ client/src/components/datos/CoursesPreview/CoursesPreview.jsx
@@ -14,56 +14,64 @@ function CoursesPreview ( { courses } ) {
 
     //component:
     return (
-            <div className = "courses-container"> 
-                <Swiper
-                    enabled={true}
-                    modules={[EffectCoverflow, Pagination, Navigation]}
-                    // effect={"coverflow"}
-                    // spaceBetween={100}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    loop={true}
-                    slidesPerView={1}
-                    keyboard={{
-                        enabled: true,
-                    }}
-                    coverflowEffect={{
-                        rotate: 0,
-                        stretch: 0,
-                        depth: 200,
-                        modifier: 2.5,
-                        slideShadows: false
-                    }}
-                    mousewheel
-                    pagination={{
-                        el:".swiper-pagination",
-                        clickable: true
-                    }}
-                    allowSlideNext={true}
-                    navigation={{
-                        prevEl:".swiper-button-prev",
-                        nextEl:".swiper-button-next",
-                    }}
-                    className="swiper-container"
-                    nested={true}
-                >
-                    {
-                        courses.map((course) => (
-                            <SwiperSlide key={course.id} style={{ margin: 0 }}>
-                                <CoursePreview
-                                    id={course.id}
-                                    title={course.title}
-                                    description={course.description}
-                                    rating={course.rating}
-                                    free={course.free}
-                                    language={course.language}
-                                    imageURL={course.imageURL}
-                                    className="courseCardContainer"
-                                />
-                            </SwiperSlide>
-                        ))
-                    }
-                </Swiper>
+            <div className = "coursesSwiperComponent"> 
+                <div className="navigation-container">
+                    <Swiper
+                        enabled={true}
+                        modules={[EffectCoverflow, Pagination, Navigation]}
+                        effect={"coverflow"}
+                        spaceBetween={100}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        loop={true}
+                        slidesPerView={1}
+                        keyboard={{
+                            enabled: true,
+                        }}
+                        coverflowEffect={{
+                            rotate: 0,
+                            stretch: 0,
+                            depth: 200,
+                            modifier: 2.5,
+                            slideShadows: false
+                        }}
+                        mousewheel
+                        pagination={{
+                            el:".swiper-pagination",
+                            clickable: true
+                        }}
+                        allowSlideNext={true}
+                        navigation={{
+                            prevEl:".swiper-button-prev",
+                            nextEl:".swiper-button-next",
+                        }}
+                        className="swiper-container"
+                        nested={true}
+                    >
+                        {
+                            courses.map((course) => (
+                                <SwiperSlide key={course.id} style={{ margin: 0 }}>
+                                    <CoursePreview
+                                        id={course.id}
+                                        title={course.title}
+                                        description={course.description}
+                                        rating={course.rating}
+                                        free={course.free}
+                                        language={course.language}
+                                        imageURL={course.imageURL}
+                                        className="courseCardContainer"
+                                    />
+                                </SwiperSlide>
+                            ))
+                        }
+                        {/* <div className="navigation-container">
+                        </div> */}
+                    </Swiper>
+                    {/* <div className="navigation-container2">
+                        <button className="swiper-button-next" />
+                        <button className="swiper-button-prev" />
+                    </div> */}
+                </div>
                 <div className="swiper-pagination">
                     {
                         courses.map((course, index) => (
@@ -71,10 +79,6 @@ function CoursesPreview ( { courses } ) {
                         ))
                     }
                 </div>
-                {/* <div className="navigation-container">
-                    <button className="swiper-button-prev" />
-                    <button className="swiper-button-next" />
-                </div> */}
             </div>
     )
 }
