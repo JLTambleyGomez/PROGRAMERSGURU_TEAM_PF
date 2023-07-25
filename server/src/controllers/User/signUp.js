cf9113c3397b741bf0d88dd0b3d5bbd4b79aa914
koke <jorgetambleygomez@gmail.com>
2023-07-25 18:18:01 -0400

    francisco

--- server/src/controllers/User/signUp.js
+++ server/src/controllers/User/signUp.js
@@ -8,7 +8,6 @@ const signUp = async (req, res) => {
     try {
         const { email, name, picture } = req.body;
         console.log("este es el signup");
-        console.log(req.body);
         if (!email && !name)
             return res
                 .status(404)
