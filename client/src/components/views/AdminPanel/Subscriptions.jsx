b7be447316c1e71baf22e68672ee6a048d24ad2f
MBritez07 <britezmicaela2@gmail.com>
2023-07-26 04:32:22 -0300

    25/07

--- client/src/components/views/AdminPanel/Subscriptions.jsx
+++ client/src/components/views/AdminPanel/Subscriptions.jsx
@@ -11,7 +11,7 @@ import {
 import { validateSuscription } from "./validate";
 import { Table } from "react-bootstrap";
 import {SubirImagenSuscription} from './SubirImagenSuscription'
-
+import styles from "./Suscriptions.module.css"
 
 //_________________________module_________________________
 const Subscriptions = () => {
@@ -308,40 +308,38 @@ const Subscriptions = () => {
                 ) : (
                     <></>
                 )}
-              
-<Table striped bordered hover >
-                
- 
-                <tbody >
+                <Table className={`${styles.Tabla} table table-striped table-bordered table-hover`}>
+                 <tbody >
                 <thead >
-                  <tr>
-                    <th>#</th>
-                    <th>Nombre</th>
-                    <th>Acción</th>
-                  </tr>
-                </thead>
-                
-                {!!subscriptions.length &&
+                   <tr><div className={styles.title}>
+                   <p>#</p>
+                    <p>Nombre</p>
+                    <p>Descripción</p>
+                    <p>Tipo de Suscripcion</p>
+                    <p>Precio</p>
+                    <p>Acciones</p>
+                   </div>
+                   </tr>
+                 </thead>
+                 {!!subscriptions.length &&
                     subscriptions?.map((subscription) => (
-                      
-                    <tr key={subscription.id}>
+                     <tr key={subscription.id}>
                       <td>{subscription.id}</td>
                       <td> {subscription.title}</td>
                       <td> {subscription.description}</td>
                             <td>{subscription.type}</td>
                             <td>Precio: {subscription.price}</td>
                             <td>
-                            <button   onClick={() => handleDeleteSubscription(subscription.id)}
-                            >
-                                Eliminar
-                            </button></td>
+                            <button onClick={() => handleDeleteSubscription(subscription.id)} className={styles.deleteButton}>
+                              <svg xmlns="http://www.w3.org/2000/svg" className={styles.bin} viewBox="0 0 16 16">
+                             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                          </svg></button>
+                            </td>
                             <td>
-                            <button
-                                onClick={handleEditSubscription}
-                                value={subscription.id}
-                            >
-                                Editar
-                            </button></td>
+                            <button onClick={handleEditSubscription} value={subscription.id} className={styles.modificarButton}><svg xmlns="http://www.w3.org/2000/svg" className={styles.mod} width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 21v-4a4 4 0 1 1 4 4h-4"></path><path d="M21 3a16 16 0 0 0 -12.8 10.2"></path><path d="M21 3a16 16 0 0 1 -10.2 12.8"></path><path d="M10.6 9a9 9 0 0 1 4.4 4.4"></path>
+                               </svg></button>
+                            </td>
                      
                     </tr>
                   ))}
