659a3bb4dffe981be3d1f9c4211015dbd92481c5
koke <jorgetambleygomez@gmail.com>
2023-07-26 19:05:59 -0400

    fixing2

--- client/src/components/views/Cart/Cart.jsx
+++ client/src/components/views/Cart/Cart.jsx
@@ -233,10 +233,10 @@ function Cart() {
                                     )}
                                     {cargasimulada &&
                                     <div className={styles.cargasimulada}><p className={styles.cargandotxt}>Cargando Pagos
-                                     
+                                     <div className={styles.icon2}>
                                      <div className={styles.icon}>
                                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-rotate-clockwise-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 4.55a8 8 0 0 1 6 14.9m0 -4.45v5h5"></path><path d="M5.63 7.16l0 .01"></path><path d="M4.06 11l0 .01"></path><path d="M4.63 15.1l0 .01"></path><path d="M7.16 18.37l0 .01"></path><path d="M11 19.94l0 .01"></path>
-                                        </svg>
+                                        </svg></div>
                                     </div></p></div> }
                                     {carrovacio && 
                                     <div className={styles.cargasimulada} ><p className={styles.cargandotxt}>Primero debes agregar productos</p></div>}
