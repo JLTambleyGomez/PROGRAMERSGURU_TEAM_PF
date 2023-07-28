5b070562f128f1a7f865d6aff5043f009e31823b
SebastianMartel <martel.ssebastian@gmail.com>
2023-07-27 21:41:41 -0500

    about styles revised

--- client/src/components/views/About/About.jsx
+++ client/src/components/views/About/About.jsx
@@ -1,6 +1,5 @@
 import theme from "../../../theme/theme";
 
-
 import s from "./About.module.css"
 import Micaela from "../../../assets/img4/Micaela.png"
 import Francisco from "../../../assets/img4/Francisco.png"
@@ -11,7 +10,9 @@ import Sol from "../../../assets/img4/Sol.png"
 import Sebastian from "../../../assets/img4/Sebastian.png"
 
 
+//_________________________module_________________________
 const About = () => {
+
     return (
         <main className={`${s.component} ${s[theme("component")]}`}>
             <div className={s.title}>
@@ -23,72 +24,47 @@ const About = () => {
             <div className={s.content}>
                 <div className={`${s.team}`}>
                     <div className={`${s.imageWrapper} ${s.francisco}`}>
-                        <img src={Francisco} alt="Image" />
+                        <img src={Francisco} alt="Francisco Passetti" />
                     </div>
                     <label>Francisco Passetti</label>
                 </div>
-                <div className={`${s.team} ${s.gianina}`}>
-                    <div className={s.imageWrapper}>
-                    {/* <label>Gianina Crespo</label>s */}
-                        <img src={Gianina} alt="Image" />
+                <div className={`${s.team}`}>
+                    <div className={`${s.imageWrapper} ${s.gianina}`}>
+                        <img src={Gianina} alt="Gianina Crespo" />
                     </div>
+                    <label>Gianina Crespo</label>
                 </div>
-                <div className={`${s.team} ${s.jorge}`}>
-                    <div className={s.imageWrapper}>
-                    {/* <label>Jorge Tambley</label>s */}
-                        <img src={Jorge} alt="Image" />
+                <div className={`${s.team}`}>
+                    <div className={`${s.imageWrapper} ${s.jorge}`}>
+                        <img src={Jorge} alt="Jorge Tambley" />
                     </div>
+                    <label>Jorge Tambley</label>
                 </div>
-                <div className={`${s.team} ${s.micaela}`}>
-                    <div className={s.imageWrapper}>
-                    {/* <label>Micaela Britez</label>s */}
-                        <img src={Micaela} alt="Image" />
+                <div className={`${s.team}`}>
+                    <div className={`${s.imageWrapper} ${s.micaela}`}>
+                        <img src={Micaela} alt="Micaela Britez" />
                     </div>
+                    <label>Micaela Britez</label>
                 </div>
-                <div className={`${s.team} ${s.santiago}`}>
-                    <div className={s.imageWrapper}>
-                    {/* <label>Santiago Calderon</label>s */}
-                        <img src={Santiago} alt="Image" />
+                <div className={`${s.team}`}>
+                    <div className={`${s.imageWrapper} ${s.santiago}`}>
+                        <img src={Santiago} alt="Santiago Calderon" />
                     </div>
+                    <label>Santiago Calderon</label>
                 </div>
-                <div className={`${s.team} ${s.sebastian}`}>
-                    <div className={s.imageWrapper}>
-                    {/* <label>Sebastian Menacho</label>s */}
-                        <img src={Sebastian} alt="Image" />
+                <div className={`${s.team}`}>
+                    <div className={`${s.imageWrapper} ${s.sebastian}`}>
+                        <img src={Sebastian} alt="Sebastian Menacho" />
                     </div>
+                    <label>Sebastian Menacho</label>
                 </div>
-                <div className={`${s.team} ${s.sol}`}>
-                    <div className={s.imageWrapper}>
-                    {/* <label>Sol Benevent</label>s */}
-                        <img src={Sol} alt="Image" />
+                <div className={`${s.team}`}>
+                    <div className={`${s.imageWrapper} ${s.sol}`}>
+                        <img src={Sol} alt="Sol Benevent" />
                     </div>
+                    <label>Sol Benevent</label>
                 </div>
             </div>
-                {/* <div className={s.card1}>
-                    <div className={s.imageWrapper}>
-                        <img src={Francisco} className={`${s.img} ${s.francisco}`}/>
-                    </div>
-                </div>
-                <div className={s.team}>
-                    <img src={Gianina} className={`${s.img} ${s.gianina}`}/>
-                </div>
-                <div className={s.team}>
-                    <img src={Jorge} className={`${s.img} ${s.jorge}`}/>
-                </div>
-                <div className={s.team}>
-                        <img src={Micaela} className={`${s.img} ${s.micaela}`}/>
-                </div>
-                <div className={s.team}>
-                    <img src={Santiago} className={`${s.img} ${s.santiago}`}/>
-                </div>
-                <div className={s.team}>
-                    <img src={Sebastian} className={`${s.img} ${s.sebastian}`}/>
-                </div>
-                <div className={s.team}>
-                    <div className={s.card}>
-                        <img src={Sol} className={`${s.img} ${s.sol}`}/>
-                    </div>
-                </div> */}
         </main>
     )
 }
